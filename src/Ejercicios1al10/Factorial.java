339159bda257fbd0277e78715c8db6f8f41698ab
Jordy Caichihua <jcaichihuav@gmail.com>
2023-08-17 21:38:33 -0500

    Enter

--- src/Ejercicios1al10/Factorial.java
+++ src/Ejercicios1al10/Factorial.java
@@ -6,6 +6,7 @@ public class Factorial {
     public  static void main(String[] args) {
         resolverFactorial();
 
+        
     }
     static void resolverFactorial(){
         Scanner input = new Scanner(System.in);
